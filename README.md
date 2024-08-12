ed8ef7afdae291fe3f3aeb95402083a062b45347
Mattia Corvaglia <corvagliamattia@gmail.com>
2024-08-12 21:20:41 +0200

    Added init function

--- README.md
+++ README.md
@@ -6,38 +6,63 @@ Based on `openssl enc`.
 
 ---
 
+## Initialization
+
+1. Clone this repository into your home and rename the folder, e.g. `.pwd-mng`.
+2. Add an alias into your `.zshrc` or `.bash_profile` configuration file to be able to run the script from everywhere:
+  ```sh
+  # add this line into .zshrc
+  alias pass='~/.pwd-mng/pass.sh'
+
+  # Save & close the file, then update the shell environment
+  source .zshrc
+  ```
+3. Edit the following variables of the `pass.sh` script:
+  ```
+  LOCALE_PATH="/path/to/locale/folder"
+  REMOTE_PATH="/path/to/dropbox/"
+  ```
+4. Create a new file, e.g. `pwd.txt`, and insert into it all the passwords you want to protect (you can use Markdown format or every other depending your preferences).
+5. Run `pass -i ` to generate the first version of your encrypted password file.
+6. Run `pass -c` to clean the folder.
+
+---
+
 ## Features
 
 ```
-    -h, --help                    Print help.
+    -h, --help                    Print this help.
 
     -v, --version                 Print script version.
 
+    -i [file], --init [file]      Initialize the password file providing
+                                  an input file to be encrypted.
+
     -l, --list                    List the content of the password folder.
 
-    -g, --get                     Get the decrypted password file for reading.
+    -g, --get                     Decrypt the password file for reading.
 
-    -e, --edit                    Get the decrypted password file for editing.
+    -e, --edit                    Decrypt the password file for editing.
 
-    -s, --save                    Save the password file just edited.
+    -s, --save                    Save changes encrypting the password file
+                                  just edited, locally and remotly.
+                                  (Remotly versioned with current date)
 
     -c, --clean                   Remove all the temporary files.
 
     -f [name], --find [name]      Search for name in the password file.
 
-    -r, --restore                 Restore the password folder to the
+    -r, --restore                 Restore the password file to the
                                   previous state.
 
 ```
 
 ---
 
-## Configuration
+## Remote versioned backup
 
-Initialize the following variable:
-
+Every time you save changes locally, a copy of the encrypted file is made in the `REMOTE_PATH`, with this format:
 ```
-BASE_PATH="/path/to/files/"
-REMOTE_FILE="/path/to/dropbox/.pwd_enc.txt"
+/path/to/dropbox/2024-07-23_pwd.aes
 ```
 
--- pass.sh
+++ pass.sh
@@ -7,18 +7,21 @@
 # ------------------------------------------------------------------------------
 
 SCRIPT_NAME="$(basename ${0})"
-BASE_PATH="/path/to/files/"
-ENC_FILE="$BASE_PATH.pwd_enc.txt"
-BACKUP_FILE="$BASE_PATH.pwd_bkp.txt"
-TEMP_FILE="$BASE_PATH.pwd_tmp.txt"
-GHOST_FILE="$BASE_PATH.pwd_chk.txt"
-REMOTE_FILE="/path/to/dropbox/.pwd_enc.txt"
+
+LOCALE_PATH="/path/to/locale/folder"
+REMOTE_PATH="/path/to/dropbox/"
+REMOTE_SUFFIX="_pwd.aes"
+
+FILE_ENC="$LOCALE_PATH.pwd.aes"
+FILE_TMP="$LOCALE_PATH.tmp.txt"
+FILE_BKP="$LOCALE_PATH.bkp.aes"
+FILE_CHK="$LOCALE_PATH.chk.txt"
 
 # ------------------------------------------------------------------------------
 function usage {
 echo "
 SYNOPSIS
-    $SCRIPT_NAME [-h|-g|-c|-e|-s|-l] [-f] [arg ...]
+    $SCRIPT_NAME [-h|-g|-c|-e|-s|-l] [-f|-i [arg ...]]
 
 DESCRIPTION
     Bash client for managing password file.
@@ -29,19 +32,24 @@ OPTIONS
 
     -v, --version                 Print script version.
 
+    -i [file], --init [file]      Initialize the password file providing
+                                  an input file to be encrypted.
+
     -l, --list                    List the content of the password folder.
 
-    -g, --get                     Get the decrypted password file for reading.
+    -g, --get                     Decrypt the password file for reading.
 
-    -e, --edit                    Get the decrypted password file for editing.
+    -e, --edit                    Decrypt the password file for editing.
 
-    -s, --save                    Save the password file just edited.
+    -s, --save                    Save changes encrypting the password file
+                                  just edited, locally and remotly.
+                                  (Remotly versioned with current date)
 
     -c, --clean                   Remove all the temporary files.
 
     -f [name], --find [name]      Search for name in the password file.
 
-    -r, --restore                 Restore the password folder to the
+    -r, --restore                 Restore the password file to the
                                   previous state.
 
 EXAMPLES
@@ -72,20 +80,19 @@ function invalid_opt {
 
 # ------------------------------------------------------------------------------
 function decrypt {
-  # Get the passphrase
   PASSPRHASE=""
   echo -n "Enter the password: "
   read -s PASSPRHASE
   echo ""
 
   # Decrypt the file
-  openssl enc -aes-256-cbc -base64 -md sha-256 -d -in $ENC_FILE -out $TEMP_FILE -k $PASSPRHASE 2> /dev/null
+  openssl enc -aes-256-cbc -base64 -d -in $FILE_ENC -out $FILE_TMP -k $PASSPRHASE 2> /dev/null
 
   # Check the exit status
   if [ $? -ne 0 ]
   then
     echo "Invalid password!"
-    rm $TEMP_FILE
+    rm $FILE_TMP 2> /dev/null
     unset PASSPRHASE
     exit 1
   fi
@@ -93,70 +100,70 @@ function decrypt {
 
 # ------------------------------------------------------------------------------
 function encrypt {
-  # Get the passphrase
   PASSPRHASE=""
   echo -n "Enter the password: "
   read -s PASSPRHASE
   echo ""
 
   # Check the passphrase
-  openssl enc -aes-256-cbc -base64 -md sha-256 -d -in $BACKUP_FILE -out $GHOST_FILE -k $PASSPRHASE 2> /dev/null
+  openssl enc -aes-256-cbc -base64 -d -in $FILE_BKP -out $FILE_CHK -k $PASSPRHASE 2> /dev/null
 
   # Check the exit status
   if [ $? -ne 0 ]
   then
     echo "Invalid password!"
+    rm $FILE_CHK
     unset PASSPRHASE
     exit 1
   else
     # Encrypt the new secret file
-    openssl enc -aes-256-cbc -base64 -md sha-256 -salt -in $TEMP_FILE -out $ENC_FILE -k $PASSPRHASE 2> /dev/null
+    openssl enc -aes-256-cbc -base64 -salt -in $FILE_TMP -out $FILE_ENC -k $PASSPRHASE 2> /dev/null
 
     if [ $? -ne 0 ]
     then
       echo "Error while encrypting the new file."
-      rm $GHOST_FILE
-      rm $ENC_FILE
+      rm $FILE_CHK
+      rm $FILE_ENC
       unset PASSPRHASE
       exit 1
     fi
   fi
 
-  rm $TEMP_FILE
-  rm $GHOST_FILE
-  rm $BACKUP_FILE
+  rm $FILE_TMP
+  rm $FILE_CHK
+  rm $FILE_BKP
 }
 
 # ------------------------------------------------------------------------------
 function show {
   decrypt
-  open $TEMP_FILE
+  open $FILE_TMP
   echo "Remember to run \"pass -c|--clean\" to clean the temporary files."
 }
 
 # ------------------------------------------------------------------------------
 function doctor {
-  ls -lart $BASE_PATH
+  ls -FGlAhp $LOCALE_PATH
 }
 
 # ------------------------------------------------------------------------------
 function clean {
-  rm $TEMP_FILE
+  rm $FILE_TMP
   echo "The password folder has been cleaned."
 }
 
 # ------------------------------------------------------------------------------
 function restore {
-  mv $BACKUP_FILE $ENC_FILE
-  rm $TEMP_FILE
+  mv $FILE_BKP $FILE_ENC
+  rm $FILE_TMP
   echo "The password file has been restored to the previous state."
 }
 
 # ------------------------------------------------------------------------------
 function edit {
   decrypt
-  mv $ENC_FILE $BACKUP_FILE
-  open $TEMP_FILE
+  mv $FILE_ENC $FILE_BKP
+  open $FILE_TMP
   echo "Remember to run \"pass -s|--save\" for saving changes."
   echo "In case of errors, run \"pass -r|--restore\" to restore the password file."
 }
@@ -164,7 +171,8 @@ function edit {
 # ------------------------------------------------------------------------------
 function save {
   encrypt
-  cp $ENC_FILE $REMOTE_FILE
+  NOW=$(date '+%Y-%m-%d')
+  cp $FILE_ENC $REMOTE_PATH$NOW$REMOTE_SUFFIX
   echo "The password file has been updated."
 }
 
@@ -172,18 +180,38 @@ function save {
 function find {
   if [ -z "$1" ]
   then
-    echo "Invalid option. You must provide a name after -f option."
+    echo "Invalid option. You must provide a name after -f|--find option."
     exit 1
   fi
   decrypt
-  cat $TEMP_FILE | grep $1
-  rm $TEMP_FILE
+  cat $FILE_TMP | grep $1
+  rm $FILE_TMP
+}
+
+# ------------------------------------------------------------------------------
+function init {
+  if [ -z "$1" ]
+  then
+    echo "Invalid option. You must provide a file after -i|--init option."
+    exit 1
+  fi
+  mv $1 $FILE_TMP
+  openssl enc -aes-256-cbc -base64 -salt -in $FILE_TMP -out $FILE_ENC 2> /dev/null
+  if [ $? -ne 0 ]
+  then
+    echo "Error while encrypting the new file."
+    rm $FILE_TMP
+    unset PASSPRHASE
+    exit 1
+  fi
+  rm $FILE_TMP
 }
 
 # ------------------------------------------------------------------------------
 while [ "$#" -gt 0 ]
 do
   case "$1" in
+    -i|--init) init "$2" ;;
     -f|--find) find "$2" ;;
     -g|--get) show ;;
     -c|--clean) clean ;;
@@ -201,11 +229,12 @@ done
 
 # Unset all the variables
 unset PASSPRHASE
-unset BASE_PATH
-unset ENC_FILE
-unset TEMP_FILE
-unset BACKUP_FILE
-unset GHOST_FILE
-unset REMOTE_FILE
+unset LOCALE_PATH
+unset FILE_ENC
+unset FILE_TMP
+unset FILE_BKP
+unset FILE_CHK
+unset REMOTE_PATH
+unset REMOTE_SUFFIX
 
 exit
